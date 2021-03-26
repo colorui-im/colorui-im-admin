1649a7e5c874be0136877ef5ed4598cc9b4a99d3
jcc <370234915@qq.com>
2021-03-26 21:18:08 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,32 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-       $users =  \App\Models\User::factory(10)->create();
+        $users =  \App\Models\User::factory(10)->create();
         $groups = \App\Models\Group::factory(10)->create();
         $divideGroups = \App\Models\DivideGroup::factory(10)->create();
 
 
-        foreach ($users as $k=>$user){
-
-            foreach ($groups as $k1=>$group){
-                if($k==$k1){
+        foreach ($users as $k=>$user) {
+            foreach ($groups as $k1=>$group) {
+                if ($k==$k1) {
                     $user->groups()->save($group);
                     $group->users()->sync($users->pluck('id')->toArray());
                     break;
                 }
             }
 
-            foreach ($divideGroups as $k2=>$divideGroup){
-                if($k==$k2){
+            foreach ($divideGroups as $k2=>$divideGroup) {
+                if ($k==$k2) {
                     $user->divideGroups()->save($group);
                     $divideGroup->users()->sync($users->pluck('id')->toArray());
                     break;
                 }
             }
-
-
-       
-
+        }
     }
 }
