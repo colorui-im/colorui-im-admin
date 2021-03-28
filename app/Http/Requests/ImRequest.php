52e36cbc203a81373ec9da50bda98618b73fe534
jcc <370234915@qq.com>
2021-03-28 13:35:16 +0800

    上传图片

--- app/Http/Requests/ImRequest.php
+++ app/Http/Requests/ImRequest.php
@@ -35,7 +35,7 @@ class ImRequest extends FormRequest
                 //{type/from/to/data/self}
                 $rules = [
                     'type' => ['required','in:friend,group'],
-                    'message_type'=> ['required','in:text'],
+                    'message_type'=> ['required','in:text,image'],
 
                     'to' => ['required'],
                     'data' => ['required']
