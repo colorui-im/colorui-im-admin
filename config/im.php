c01a94c8af337e6fa777e2b1b221e2927539b1e5
jcc <370234915@qq.com>
2021-04-07 16:28:04 +0800

    update

--- config/im.php
+++ config/im.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'save_message'=> env('IM_SAVE_MESSAGE',true)
+    'save_message'=> env('IM_SAVE_MESSAGE',true),
+    'register_address'=> env('IM_REQISTER_ADDRESS','172.17.0.1:1240')
 ];
